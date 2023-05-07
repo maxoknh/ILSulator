63a0e18e4865f8d85db4256fc804d7e9ed2f960c
maxoknh <tckraus@yahoo.com>
2023-05-07 00:22:33 -0400

    Clean up code. Begin findPeakData to replace printPeaks,findPeaks, and splitPeaks.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import scipy.integrate as integrate
+import scipy.signal as signal
 import numpy as np
 import matplotlib.pyplot as plt
 import statistics as stat
@@ -9,7 +10,7 @@ start = 0
 stop = 1000000
 step = 100
 UScalar = 5
-# Factory Demand Input
+# Planet Demand per Second
 ips = 44
 # Vessel Inputs
 VCount = 100
@@ -31,8 +32,7 @@ TB = 27636
 dRA = 332520
 dRB = 438680
 
-
-# Conversion Values
+# Conversion Values (d)
 VSpeed = dVSpeed / np.power(10, UScalar)
 RA = dRA / np.power(10, UScalar)
 RB = dRB / np.power(10, UScalar)
@@ -70,18 +70,18 @@ thetaF = findAngleDifference()
 # VFac then converts the time into items/s.
 def orbitSim(t):
     return (VFac / (4 * VDelay + 2 * ((np.sqrt(np.power(
-            RB * np.sin(lambdaB * t) - RA * np.sin(lambdaA * t) * np.cos(thetaF), 2) + np.power(
-            RB * np.cos(lambdaB * t) - RA * np.cos(lambdaA * t), 2) + np.power(
-            0 - RA * np.sin(lambdaA * t) * np.sin(thetaF), 2)) - 2 * DAdjust) / VSpeed)))
+        RB * np.sin(lambdaB * t) - RA * np.sin(lambdaA * t) * np.cos(thetaF), 2) + np.power(
+        RB * np.cos(lambdaB * t) - RA * np.cos(lambdaA * t), 2) + np.power(
+        0 - RA * np.sin(lambdaA * t) * np.sin(thetaF), 2)) - 2 * DAdjust) / VSpeed)))
 
 
-# print("%f" % orbitSim(0))
+# print("%f" % orbitSim(1))
 
 
 # Build the time step array
 T = np.arange(start, stop, step, np.int64)
 
-
+"""
 # Integrates the orbital sim according to an array of input times
 # If the step size reaches 500,000, the integral will be broken up into 500,000 step-sized chunks to preserve precision
 def F(t):
@@ -92,24 +92,24 @@ def F(t):
         if chunk > 0:
             for v in range(chunk):
                 a = chunkSize * v
-                b = chunkSize * (v+1)
+                b = chunkSize * (v + 1)
                 y = integrate.quad(orbitSim, a, b)[0]
-                res[i] = res[i]+y
-            yf = integrate.quad(orbitSim, chunkSize*chunk, val)[0]
-            res[i] = res[i]+yf
+                res[i] = res[i] + y
+            yf = integrate.quad(orbitSim, chunkSize * chunk, val)[0]
+            res[i] = res[i] + yf
         else:
             y = integrate.quad(orbitSim, 0, val)[0]
             res[i] = y
         res[i] = res[i] - (ips * val)
     return res
-
+"""
 
 def Fa(t):
     res = np.zeros_like(t)
     marker = 0
     for i, val in enumerate(t):
         if i > 0:
-            res[i] = res[i-1] + integrate.quad(orbitSim, marker, val)[0]
+            res[i] = res[i - 1] + integrate.quad(orbitSim, marker, val)[0]
             res[i] = res[i] - (ips * (val - marker))
             marker = marker + step
         else:
@@ -122,22 +122,23 @@ def Fb(t):
     marker = 0
     for i, val in enumerate(t):
         if i > 0:
-            res[i] = res[i-1] + integrate.quad(orbitSim, marker, val)[0]
+            res[i] = res[i - 1] + integrate.quad(orbitSim, marker, val)[0]
             marker = marker + step
         else:
             res[i] = integrate.quad(orbitSim, marker, val)[0]
     return res
 
 
-
+"""
 def test():
     out = F(T)
     for i, val in enumerate(T):
         print("%f, %f" % (val, out[i]))
-
+"""
 
 OrbitWithNegative = Fa(T)
 OrbitWithoutNegative = Fb(T)
+
 
 def findPeaks(Orbit):
     mark = 0
@@ -164,9 +165,9 @@ def findPeaks(Orbit):
 
 
 mixedPeaks = findPeaks(OrbitWithNegative)
+
+
 def printPeaks():
-    dbp = 0
-    dap = 0
     avgBef = 0
     avgBefCount = 0
     avgAfter = 0
@@ -174,7 +175,7 @@ def printPeaks():
     maxBef = 0
     maxAft = 0
     for i, val in enumerate(mixedPeaks[0]):
-        if i == mixedPeaks[0].size-1 or val > mixedPeaks[0][i + 1]:
+        if i == mixedPeaks[0].size - 1 or val > mixedPeaks[0][i + 1]:
             if i != 0:
                 dbp = val - mixedPeaks[0][i - 1]
                 avgBef += dbp
@@ -182,19 +183,27 @@ def printPeaks():
                 # print("Difference before peak: %d" % dbp)
                 if dbp > maxBef:
                     maxBef = dbp
-            if i < mixedPeaks[0].size-1:
+            if i < mixedPeaks[0].size - 1:
                 dap = val - mixedPeaks[0][i + 1]
                 avgAfter += dap
                 avgAfterCount += 1
                 # print("Difference after peak: %d" % dap)
                 if dap > maxAft:
                     maxAft = dap
-    print("Average difference pre peak: %d" % (avgBef/avgBefCount))
-    print("Average difference post peak: %d" % (avgAfter/avgAfterCount))
+    print("Average difference pre peak: %d" % (avgBef / avgBefCount))
+    print("Average difference post peak: %d" % (avgAfter / avgAfterCount))
     print("Maximum difference pre peak: %d" % maxBef)
     print("Maximum difference post peak: %d" % maxAft)
-    print("Average item gain per cycle: %d" % (avgBef/avgBefCount - avgAfter/avgAfterCount))
+    print("Average item gain per cycle: %d" % (avgBef / avgBefCount - avgAfter / avgAfterCount))
 
+
+def findPeakData(Orbit):
+    maxim = signal.find_peaks(Orbit)
+    minim = signal.find_peaks(-Orbit)
+    if (minim[0].size != 0 and maxim[0].size != 0):
+    avgMax =
+    avgMin =
+    avgTroughToPeak =
 
 def splitPeaks():
     highs = np.zeros_like(mixedPeaks[0])
@@ -203,27 +212,24 @@ def splitPeaks():
     lowInd = np.zeros_like(mixedPeaks[0])
     for i, val in enumerate(mixedPeaks[0]):
         if i % 2 == 0:
-            highs[i//2] = val
-            highInd[i//2] = mixedPeaks[1][i]
+            highs[i // 2] = val
+            highInd[i // 2] = mixedPeaks[1][i]
         else:
-            lows[i//2] = val
-            lowInd[i//2] = mixedPeaks[1][i]
+            lows[i // 2] = val
+            lowInd[i // 2] = mixedPeaks[1][i]
     return [(np.trim_zeros(highs), np.trim_zeros(highInd)), (np.trim_zeros(lows), np.trim_zeros(lowInd))]
 
 
 peaks = splitPeaks()
 highPeaks = peaks[0]
 lowPeaks = peaks[1]
-#print("Average time between peaks: %d" % np.average(np.append(highPeaks[1], lowPeaks[1])))
+# print("Average time between peaks: %d" % np.average(np.append(highPeaks[1], lowPeaks[1])))
 slope, intercept = stat.linear_regression(OrbitWithNegative, T, proportional=True)
 print("The average increase in storage is: %f items/s" % slope)
 
-
-#printPeaks()
+# printPeaks()
 # test()
 plt.plot(T, Fa(T))
 plt.show()
-#plt.plot(findPeaks())
-#plt.show()
-
-
+# plt.plot(findPeaks())
+# plt.show()
