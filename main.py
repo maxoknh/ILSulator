7c49a88a08cafc5753e4d5d0dd93b5bc812ff9dc
maxoknh <tckraus@yahoo.com>
2024-04-14 19:18:15 -0400

    Update main.py
    Added itemStartingBuffer for the receiving planet if it already has some buffer established
    Converted the time table into a 64 bit float, improving accuracy after time parameter changes
    This also fixed a bug where high step counts would cause the output data to be all 0's
    UScalar default was lowered to 0, and is deprecated now
    Lowered default step count to greatly improve performance, comes with ~1-5% loss in accuracy
    Updated example inputs to be a large system again
    Updated output prints to be clearer and include more information
    Added more output prints

--- main.py
+++ main.py
@@ -4,44 +4,57 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Only works with concentric orbits.
+# How to use:
+# Choose your planets, copy their orbital stats into the area below
+# Set your desired transfer rate between the planets
+# Set your cargo vessel stats, which can be found on the right side of the upgrade tab under 'Interstellar Vessel Upgrades'
+# Choose your number of cargo vessels you wish to use
+# Repeat until graph does not intersect with zero.
 
 # Inputs
 
 # Item Demand per Second
-ips = 24
-# Vessel Stats
-VCount = 6
-dVSpeed = 600
-VCargo = 200
+ips = 48
+# Vessel Count
+VCount = 54
+# Vessel Speed
+dVSpeed = 1200
+# Vessel Cargo Count
+VCargo = 600
+# Items already in buffer on receiving planet
+itemStartingBuffer = 0
 
 # Planet A:
 # Orbit Radius (AU)
-dRA = .105
+dRA = 8.313
 # Orbital Period (seconds)
-TA = 2048
+TA = 18238
 # Orbit Inclination (degrees, minutes)
-dInclinationA = 11
-dInclinationAminutes = 47
+dInclinationA = 6
+dInclinationAminutes = 13
 # Longitude of Ascending Node (degrees, minutes)
-dlanA = 83
-dlanAminutes = 37
+dlanA = 38
+dlanAminutes = 19
 
 # Planet B:
 # Orbit Radius (AU)
-dRB = .065
+dRB = 10.967
 # Orbital Period (seconds)
-TB = 993
+TB = 27636
 # Orbit Inclination (degrees, minutes)
-dInclinationB = 7
-dInclinationBminutes = 16
+dInclinationB = 6
+dInclinationBminutes = 53
 # Longitude of Ascending Node (degrees, minutes)
-dlanB = 42
-dlanBminutes = 41
+dlanB = 126
+dlanBminutes = 51
 
 # Simulation Time Parameters (seconds). Based on orbit with longest period.
-# If step is too fine, (~5000 steps per revolution,) output seems to break.
-revolutions = 5
-stepsPerRevolution = 2000
+# Step count can greatly affect accuracy depending on orbit size!
+# A few revolutions is usually enough to get a good enough idea, higher revolutions is better for planets with high inclinations and different LAN's.
+# Recommended step count is at least 100, accuracy seems to be ~1-5% better at 1000 based on rough testing on a large system.
+# It is not recommended to exceed 5000 steps per revolution, the program may hang for a long time.
+revolutions = 30
+stepsPerRevolution = 100
 start = 0
 stop = np.maximum(TA,TB) * revolutions
 step = stop/(revolutions * stepsPerRevolution)
@@ -50,7 +63,8 @@ step = stop/(revolutions * stepsPerRevolution)
 VDelay = 12
 dDAdjust = 2500
 
-# Divides all numbers used by 10^x. Prevents integer overflow. May lower accuracy.
+# Divides all numbers used by 10^x. May help prevent errors. May lower accuracy.
+# Not much purpose anymore, but it's here.
 UScalar = 0
 
 
@@ -91,7 +105,7 @@ thetaF = findAngleDifference()
 
 
 # Convert the parameters into parametric equations modeling the orbits, and calculates the distance between
-# them at a given time.
+# them at a given time, and then converting that distance into an item/s count.
 # DAdjust takes off some distance from each planet, and the distance is then converted into vessel travel time.
 # VDelay adds some time to account for take off and landing, VFac then converts the time into items/s.
 def itemSim(t):
@@ -104,13 +118,14 @@ def itemSim(t):
 
 
 # Build the time step array
-T = np.arange(start, stop, step, np.int64)
+T = np.arange(start, stop, step, np.float64)
 
 
 # Integrates the item/s data into an item count.
 # One accounts for the item/s demand, while the other does not
 def simWithDrain(t):
     res = np.zeros_like(t)
+    res[0] = itemStartingBuffer
     for i in range(1, t.size, 1):
         res[i] = np.maximum(res[i-1] + integrate.quad(itemSim, t[i-1], t[i])[0] - (ips * (t[i] - t[i-1])), 0)
     return res
@@ -118,6 +133,7 @@ def simWithDrain(t):
 
 def simWithoutDrain(t):
     res = np.zeros_like(t)
+    res[0] = itemStartingBuffer
     for i in range(1, t.size, 1):
         res[i] = res[i-1] + integrate.quad(itemSim, t[i-1], t[i])[0]
     return res
@@ -150,14 +166,37 @@ def findPeakData(data, t, verbo=True):
     avgMinToMax = np.average(minToMax)
     avgGainPerCycle = np.average(np.subtract(maxima[1:], maxima[0:maxima.size-1]))
     avgCycleTime = np.average(np.subtract(maximaTimes[1:], maximaTimes[0:maximaTimes.size-1]))
+
+    bufferDepletesFirstCycle = False
+    for i in range (1, maximaIndices[1], 1):
+        if data[i] == 0:
+            bufferDepletesFirstCycle = True
+            break
+
+    bufferDepletes = False
+    for i in range(maximaIndices[1], data.size, 1):
+        if data[i] == 0:
+            bufferDepletes = True
+            break
     
     if verbo:
         print("Average Peak to Peak Gain per Cycle: %.3f" % avgGainPerCycle)
-        print("Maximum needed buffer per side/Maximum Peak to Trough Loss: %d" % np.ceil(np.amax(np.abs(maxToMin))))
-        print("////////////////////////////////////////////////////////////////////")
-        print("Average needed buffer per side: %.3f" % np.abs(avgMaxToMin))
-        print("Average Trough to Peak Gain: %.3f" % np.abs(avgMinToMax))
-        print("Average Peak Cycle time: %.3f" % (avgCycleTime))
+        print("Maximum needed buffer per side (Maximum Max-to-Min Loss): %d" % np.ceil(np.amax(np.abs(maxToMin))))
+        if bufferDepletes and bufferDepletesFirstCycle:
+            print("Vessel count cannot sustain this demand!")
+        elif not bufferDepletes and bufferDepletesFirstCycle:
+            print("Vessel count sustains demand. Starting buffer may be needed.")
+        elif bufferDepletes and not bufferDepletesFirstCycle:
+            print("Vessel count cannot sustain demand. Starting buffer will delay depletion by at least one cycle.")
+        else:
+            print("Vessel count always sustains demand.")
+        print("Starting buffer needed (Max Peak - First Peak): %.3f" % (np.ceil(np.amax(np.abs(maxToMin))) - maxima[0]))
+        print("/////////////////////////////////Other Statistics///////////////////////////////////")
+        print("Vessel count: %d vessels" % VCount)
+        print("First Peak Height: %.3f items" % maxima[0])
+        print("Average needed buffer per side: %.3f items" % np.abs(avgMaxToMin))
+        print("Average Min-to-Max Gain: %.3f items" % np.abs(avgMinToMax))
+        print("Average Time Between Peaks: %.3f seconds" % (avgCycleTime))
         print("Number of maximums: %d" % (maxima.size))
         print("Number of minimums: %d" % (minima.size))
 
